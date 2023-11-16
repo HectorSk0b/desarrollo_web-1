264b7ba185e4033c54964514499451b0a10c9f44
Norbac27 <91506945+Norbac27@users.noreply.github.com>
2023-11-16 10:34:58 -0500

    Create conexion.php

---
+++ parte_2/productos/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$db_host='localhost';
+$db_nombre = 'usuarios';
+$db_usuario = 'root';
+$db_contra = "";
+
+$conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
+
+if (mysqli_connect_errno()) {
+    echo "la conexion con la base de datos ha fallado";
+    exit();
+}
+?>
